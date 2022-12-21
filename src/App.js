b20efae1f27199c094fe47899eef527374664d81
ProgrammingWithGaurav <gaurav2499kumar@gmail.com>
2022-12-21 09:47:57 +0000

    initial setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+    return (
+        <h1 className='underline text-3xl'>
+            hi App
+        </h1>
+    )
+}
+
+export default App
